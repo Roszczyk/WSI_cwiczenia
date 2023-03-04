555f11149fd5ef079e0c21918d4769112115db59
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-03-04 17:48:37 +0100

    Dokonczenie zachlannego

--- Cw1/zad1_zachlanny.py
+++ Cw1/zad1_zachlanny.py
@@ -6,6 +6,7 @@ w = np.array([8, 3, 5, 2]) #waga przedmiotów
 W = 9 #maksymalna waga plecaka
 p = np.array([16, 8, 9, 6]) #wartość przedmiotów
 
+
 ppw=[]
 optionP=[]
 
@@ -14,8 +15,31 @@ for i in range(w.size):
     ppw.append(temp_ppw)
 
 best_ppw=0
-for i in range(len(ppw)):
-    if ppw[i] > best_ppw:
-        best_ppw = ppw[i]
-        best_thing = i
+sorted_items=[]
+for j in range(len(ppw)):
+    for i in range(len(ppw)):
+        if (ppw[i] > best_ppw):
+            best_ppw = ppw[i]
+            best_thing=i
+    sorted_items.append(best_thing)
+    ppw[best_thing]=0
+    best_ppw=0
 
+print(sorted_items)
+
+currentP=0
+currentW=0
+solution=[0,0,0,0]
+for i in range(len(sorted_items)):
+    currentW=currentW+w.item(sorted_items[i])
+    if (currentW<W):
+        currentP=currentP+p.item(sorted_items[i])
+        solution[sorted_items[i]]=1
+    else:
+        break
+
+print(f"option {solution} with score {currentW}")
+
+end = time.time()
+total = end - start
+print("{0:02f}s".format(total))
