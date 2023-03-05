63e9a8883cbc50e33bc4d227f9a5644a6eb0ebd4
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-03-05 23:57:02 +0100

    just learning NumPy

---
+++ Cw1/zad1_zach_np.py
@@ -0,0 +1,37 @@
+import numpy as np
+import time
+start = time.time()
+
+w = np.array([8, 3, 5, 2]) #waga przedmiotów
+W = 9 #maksymalna waga plecaka
+p = np.array([16, 8, 9, 6]) #wartość przedmiotów
+
+
+ppw=p/w
+optionP=[]
+
+best_ppw=0
+sorted_items=[]
+for j in range(len(ppw)):
+    best_ppw = ppw.max()
+    best_thing= ppw.argmax()
+    sorted_items.append(best_thing)
+    ppw[best_thing]=0
+    best_ppw=0
+
+currentP=0
+currentW=0
+solution=[0,0,0,0]
+for i in range(len(sorted_items)):
+    currentW=currentW+w.item(sorted_items[i])
+    if (currentW<W):
+        currentP=currentP+p.item(sorted_items[i])
+        solution[sorted_items[i]]=1
+    else:
+        break
+
+print(f"option {solution} with score {currentP}")
+
+end = time.time()
+total = end - start
+print("{0:02f}s".format(total))
