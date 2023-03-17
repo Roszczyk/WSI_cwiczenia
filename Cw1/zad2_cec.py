42ceab8e8075f6fb9db6efdab8cacdb9fe029506
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-03-17 16:01:25 +0100

    Optymalizacja funkcji CEC w Jupyter Notebook

--- Cw1/zad2_cec.py
+++ Cw1/zad2_cec.py
@@ -1,10 +1,7 @@
 import numpy as np
 from autograd import grad
 import matplotlib.pyplot as plt
-from cec2017.functions import f1,f2, f3,f5
-
-def booth(x):
-    return (x[0]+2*x[1]-7)**2+(2*x[0]+x[1]-5)**2
+from cec2017.functions import f1,f2, f3
 
 MAX_X = 100
 PLOT_STEP = 0.1
@@ -14,10 +11,10 @@ y_arr = np.arange(-MAX_X, MAX_X, PLOT_STEP)
 X, Y = np.meshgrid(x_arr, y_arr)
 Z = np.empty(X.shape)
 
-wsp=0.000001
-DIMENSIONALITY = 10
+wsp=0.0001
+DIMENSIONALITY = 2
 x = np.random.uniform(-MAX_X, MAX_X, size=DIMENSIONALITY)
-q = f1
+q = f3
 dq = grad(q)
 
 for i in range(X.shape[0]):
@@ -26,11 +23,11 @@ for i in range(X.shape[0]):
 
 plt.contour(X, Y, Z, 20)
 
-for i in range (100):
+for i in range (1000):
     d=dq(x)
     xtemp=x
     x=x-wsp*d
-    print([i, x, q(x), d])
+    # print([i, x, q(x), d])
     plt.arrow(xtemp[0], xtemp[1], x[0]-xtemp[0], x[1]-xtemp[1], head_width=1, head_length=1, fc='k', ec='k')
 
 
