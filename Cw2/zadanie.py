04fec632293dda99f0bb72d4b36522a10d2add41
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-03-22 11:19:56 +0100

    Początek ćwiczenia 2 - algorytm ewolucyjny

--- Cw2/zadanie.py
+++ Cw2/zadanie.py
@@ -1,17 +1,25 @@
 import numpy as np
-from cec2017.functions import f1,f4, f5
+from cec2017.functions import f4, f5
 
-MU=20
-MUTATION_PROBABILITY=np.random.uniform(0, 1, 1)[0]
 
+#STAŁE DLA WYWOŁANIA:
+BUDGET=10000            #dotępny budżet ewaluacji funkcji celu
+MU=20                   #liczba osobników w populacji
+tmax=BUDGET/MU          #liczba iteracji
+MUTATION_PROBABILITY=np.random.uniform(0, 1, 1)[0]  #prawdopodobieństwo mutacji
 UPPER_BOUND = 100
 DIMENSIONALITY = 10
 
-x = np.random.uniform(-UPPER_BOUND, UPPER_BOUND, size=DIMENSIONALITY)
+#DEKLARACJE ZMIENNYCH
+t=0                     #dokonana liczba iteracji
+currentPop=[]           #populacja
+objFunPop=[]            #wartosci funkcji celu dla osobnikow populacji
 
-#wyznacz ocenę x
-print(x)
+#DEFINICJA OPTYMALIZOWANEJ FUNKCJI:
+q=f4
 
-q = f1(x)
-
-print(q)
+#LOSOWANIE POPULACJI POCZĄTKOWEJ:
+for i in range(MU):
+    currentPop.append(np.random.uniform(-UPPER_BOUND, UPPER_BOUND, size=DIMENSIONALITY))
+    objFunPop.append(q(currentPop[i]))
+    print(i, currentPop[i], objFunPop[i])
