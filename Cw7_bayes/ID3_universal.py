22406526ffc4f0ffcada2c9a4dc196a6df7fcf5e
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-06-08 14:10:21 +0200

    Kod mało uniwersalny, ale działa

---
+++ Cw7_bayes/ID3_universal.py
@@ -0,0 +1,249 @@
+import os
+import sys
+import numpy as np
+import copy
+
+FILE="dataforID3.txt"
+
+class Tree:
+    def __init__(self, choice,choiceValue, children, Class=None):
+        self.choice=choice
+        self.choiceValue=choiceValue
+        self.children=children
+        self.Class=Class
+
+def initFile(fileName): #odczytanie danych z pliku i stworzenie obiektów danych
+    path_to_data=os.path.join(sys.path[0], fileName)
+    file=open(path_to_data)
+    i=0
+    dataArray=[]
+    file_text=file.read()
+    file_lines=file_text.split("\n")
+    for i in range(len(file_lines)-1):
+        file_line=file_lines[i]
+        line_array=file_line.split(",")
+        dataArray.append(line_array)
+    file.close()
+    return dataArray
+
+def log2(value):
+    return np.log(value)/np.log(2)
+
+def most_frequent(List): #kod skopiowany od GeeksForGeeks
+    counter = 0
+    num = List[0]
+    for i in List:
+        curr_frequency = List.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+    return num
+
+def divideData(data): #podział zbioru danych na dane trenujące i testowe
+    trainingData=[]
+    testingData=[]
+    for i in range(len(data)):
+        chooseSet=np.random.choice([0,1],1,p=[0.6,0.4])
+        if chooseSet==0:
+            trainingData.append(data[i])
+        if chooseSet==1:
+            testingData.append(data[i])
+    return trainingData, testingData
+
+def nameValues(data, checking):      #nazwanie elementów danej kategorii
+    unique_list=[]
+    for i in range(len(data)):
+        if data[i][checking] not in unique_list:
+            unique_list.append(data[i][checking])
+    return unique_list
+
+def unique(list):
+    unique_list = []
+    for x in list:
+        if x not in unique_list:
+            unique_list.append(x)
+    return unique_list
+
+def divideByChecked(array, checking):
+    partsLists=[]
+    valuesList=nameValues(array, checking)
+    for i in range(len(valuesList)):
+        partsTemp=[]
+        for j in range(len(array)):
+            if array[j][checking]==valuesList[i]:
+                partsTemp.append(array[j])
+        partsLists.append(partsTemp)
+    return partsLists
+
+def countEntropy(array):
+    values=nameValues(array,0)
+    count=[]
+    for i in range(len(values)):
+        countTemp=0
+        for j in range(len(array)):
+            if array[j][0]==values[i]:
+                countTemp=countTemp+1
+        count.append(countTemp)
+    sumValues=0
+    entropy=0
+    for i in range(len(count)):
+        sumValues=sumValues+count[i]
+    for i in range(len(count)):
+        prob=count[i]/sumValues
+        entropy=entropy-prob*log2(prob)
+    return entropy
+
+def checkChildrenEntropy(array, checking): 
+    dividedArray=divideByChecked(array, checking)
+    entropy=[]
+    for i in range(len(dividedArray)):
+        entropy.append(countEntropy(dividedArray[i]))
+    return entropy
+
+def countInformationGain(parent, parentEntropy, checking):
+    ig=parentEntropy
+    children=divideByChecked(parent, checking)
+    childrenEntropyList=checkChildrenEntropy(parent, checking)
+    amount=[]
+    for i in range(len(children)):
+        amount.append(len(children[i]))
+    sum=0
+    for i in range(len(amount)):
+        sum=sum+amount[i]
+    for i in range(len(childrenEntropyList)):
+        ig=ig-(amount[i]/sum)*childrenEntropyList[i]
+    return ig
+
+def chooseBestPath(dataArray,dataEntropy,columns):
+    igList=[]
+    for i in range(columns):
+        igList.append(countInformationGain(dataArray,dataEntropy,i+1))
+    if len(igList)==0:
+        bestChoice=None
+    else:
+        bestChoice=np.array(igList).argmax()+1
+    return bestChoice
+
+def recurrentID3(array, arrayEntropy, columns):
+    if len(array[0])==1:
+        classValues=nameValues(array,0)
+        if len(classValues)==1:
+            classValue=classValues[0]
+        else:
+            count=[]
+            for i in range(len(array)):
+                countTemp=0
+                for j in range(len(classValues)):
+                    if array[i][0]==classValues[j]:
+                        countTemp=countTemp+1
+                count.append(countTemp)
+            classValue=classValues[np.array(count).argmax()]
+        return [Tree(None, None, None, classValue)]
+    if len(array[0])>1:
+        bestChoice=chooseBestPath(array, arrayEntropy, columns)
+        children=divideByChecked(array, bestChoice)
+        listOfSubtrees=[]
+        for i in range(len(children)):
+            tempArray=copy.deepcopy(children[i])
+            for j in range(len(tempArray)):
+                tempArray[j].pop(bestChoice)
+            tree=Tree(bestChoice, children[i][0][bestChoice], recurrentID3(tempArray, countEntropy(tempArray), len(tempArray[0])-1), None)
+            listOfSubtrees.append(tree)
+        return listOfSubtrees
+            
+    
+def predict(data, tree):
+    if tree[0].Class != None:
+        # print(tree[0].Class)
+        return tree[0].Class
+    choice=tree[0].choice
+    tempArray=copy.deepcopy(data)
+    tempArray.pop(choice)
+    setOfOptions=[]
+    for i in range(len(tree)):
+        if tree[i].choiceValue==data[choice]:
+            result=predict(tempArray, tree[i].children)
+            return result
+        setOfOptions.append(tree[i])
+    result = predict(tempArray, np.random.choice(setOfOptions, 1)[0].children)
+    return result
+
+def defineClassSet(data):
+    return nameValues(data,0)
+
+def defineInputSet(data):
+    inputSet=[]
+    for i in range(len(data[0])-1):
+        inputSet.append(nameValues(data,i+1))
+    return inputSet
+
+def toCountAverageAccuracy():
+    sumAccuracy=0
+    iterations=25
+    for i in range(iterations):
+
+        dataArray, testingData = divideData(initFile(FILE))
+        columns = len(dataArray[0]) - 1  # liczba kolumn bez klasy
+
+        tree=recurrentID3(dataArray, countEntropy(dataArray), columns)
+
+        # print(predict(testingData[3], tree))
+
+        countTestingData=len(testingData)
+        countTrue=0
+
+        for i in range(countTestingData):
+            predicted=predict(testingData[i], tree)
+            # print(predicted," - ", testingData[i][0])
+            if predicted==testingData[i][0]:
+                countTrue=countTrue+1
+
+        accuracy=countTrue/countTestingData
+
+        print("accuracy: ", accuracy)
+
+        sumAccuracy=accuracy+sumAccuracy
+
+    print("averageAccuracy: ", sumAccuracy/iterations)
+
+def toFindConfusionMatrix():
+    dataArray, testingData = divideData(initFile(FILE))
+    columns = len(dataArray[0]) - 1  # liczba kolumn bez klasy
+
+    tree=recurrentID3(dataArray, countEntropy(dataArray), columns)
+
+    countTestingData=len(testingData)
+    countTrue=0
+    classSet=defineClassSet(testingData)
+    matrix=[0,0,0,0]
+
+    for i in range(countTestingData):
+        predicted=predict(testingData[i], tree)
+        if predicted==classSet[0] and testingData[i][0]==classSet[0]:
+            matrix[0]=matrix[0]+1
+        if predicted==classSet[0] and testingData[i][0]==classSet[1]:
+            matrix[1]=matrix[1]+1
+        if predicted==classSet[1] and testingData[i][0]==classSet[1]:
+            matrix[2]=matrix[2]+1
+        if predicted==classSet[1] and testingData[i][0]==classSet[0]:
+            matrix[3]=matrix[3]+1
+
+    print(f"|accurate/predicted |{classSet[0]}    |   {classSet[1]}       |")
+    print(f"{classSet[0]}     |{matrix[0]}    |   {matrix[3]}     |")
+    print(f"{classSet[1]}     |{matrix[1]}    |   {matrix[2]}     |")
+
+def main():
+    dataArray, testingData = divideData(initFile(FILE))
+    columns = len(dataArray[0]) - 1  # liczba kolumn bez klasy
+    tree=recurrentID3(dataArray, countEntropy(dataArray), columns)
+    countTestingData=len(testingData)
+    countTrue=0
+    for i in range(countTestingData):
+        predicted=predict(testingData[i], tree)
+        # print(predicted," - ", testingData[i][0])
+        if predicted==testingData[i][0]:
+            countTrue=countTrue+1
+    accuracy=countTrue/countTestingData
+    print("accuracy: ", accuracy)
+
+main()
--- Cw7_bayes/bayes.py
+++ Cw7_bayes/bayes.py
@@ -10,13 +10,49 @@ def findNode(name, list_of_nodes):
             return list_of_nodes[i]
     return None
 
-class Node:
+class Atribute:
     def __init__(self, data):
         self.name=data[0][0]
-        self.behind=findNode(data[1], nodes)
-        self.true=data[2]
-        self.false=data[3]
-        self.forward=findNode(data[4], nodes)
+        self.behind=data[1]
+        tempList=[]
+        for i in range(len(data[2])):
+            tempList.append(float(data[2][i]))
+        self.true=tempList
+        tempList=[]
+        for i in range(len(data[3])):
+            tempList.append(float(data[3][i]))
+        self.false=tempList
+        self.forward=data[4]
+
+
+class DataLine:
+    def __init__(self, input):
+        self.input=input
+
+    def generateData(self):
+        dataline=[0]
+        for i in range(len(self.input)):
+            value=np.random.choice([True, False], p=[self.input[i].true[0], self.input[i].false[0]])
+            dataline.append(value)
+        if self.input[0].forward[0] == self.input[1].forward[0]:
+            next=findNode(self.input[0].forward[0], nodes)
+            if dataline[1]==True and dataline[2]==True:
+                value=np.random.choice([True, False], p=[next.true[0], next.false[0]])
+            if dataline[1]==True and dataline[2]==False:
+                value=np.random.choice([True, False], p=[next.true[1], next.false[1]])
+            if dataline[1]==False and dataline[2]==True:
+                value=np.random.choice([True, False], p=[next.true[2], next.false[2]])
+            if dataline[1]==False and dataline[2]==False:
+                value=np.random.choice([True, False], p=[next.true[3], next.false[3]])   
+            dataline.append(value)
+        next=findNode(next.forward[0], nodes)
+        if dataline[3]==True:
+            value=np.random.choice([True, False], p=[next.true[0], next.false[0]])
+        if dataline[3]==False:
+            value=np.random.choice([True, False], p=[next.true[1], next.false[1]])
+        dataline[0]=value
+        return dataline
+
 
 
 def initFile(fileName): #odczytanie danych z pliku i stworzenie obiektów danych
@@ -26,6 +62,7 @@ def initFile(fileName): #odczytanie danych z pliku i stworzenie obiektów danych
     dataArray=[]
     file_text=file.read()
     file_lines=file_text.split("\n")
+    amount=int(file_lines.pop(0))
     for i in range(len(file_lines)):
         file_line=file_lines[i]
         line_array=file_line.split(",")
@@ -33,12 +70,22 @@ def initFile(fileName): #odczytanie danych z pliku i stworzenie obiektów danych
             line_array[j]=line_array[j].split("+")
         dataArray.append(line_array)
     file.close()
-    return dataArray
+    return dataArray, amount
 
 
 
-data = initFile("data.txt")
+data, amount = initFile("data.txt")
+headlines=data.pop(0)
 nodes=[]
-for i in range(len(data)-1):
-    nodes.append(Node(data[i+2]))
-    print(nodes[i].behind)
+for i in range(len(data)):
+    nodes.append(Atribute(data[i]))
+inputs=[nodes[0], nodes[1]]
+
+
+savefile=open(os.path.join(sys.path[0], "dataforID3.txt"), "w")
+
+
+for i in range(amount):
+    dataline=DataLine(inputs).generateData()
+    savefile.write(f"{dataline[0]},{dataline[1]},{dataline[2]},{dataline[3]}\n")
+    
